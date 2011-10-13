8302fc10e279ef006286a3692e946600049c0a66
Allan E. Conquest <conquest@informatik.uni-bremen.de>
2011-10-13 19:16:02 +0200

    establishing interfaces. backup.

--- src/DBScan.cpp
+++ src/DBScan.cpp
@@ -2,7 +2,11 @@
 
 namespace machine_learning
 {
-
+    DBScan::DBScan(base::Pointcloud* pointcloud, int min_pts, double epsilon)
+    {
+        
+    }
+    
 	void DBScan::welcome()
 	{
 		std::cout << "This is the DBScan Algorithm. I am currently not at home. Please try again later." << std::endl;
--- src/DBScan.hpp
+++ src/DBScan.hpp
@@ -2,18 +2,26 @@
 #define _MACHINE_LEARNING_DBSCAN_HPP_
 
 #include <iostream>
+#include <vector>
+//#include <base/samples/pointcloud>
 
 namespace machine_learning
 {
+    struct DBScanResult {
+        base::samples::Pointcloud pointcloud;
+        int cluster_id;
+    }
+    
 	class DBScan
 	{
 		public: 
-			/**
-			* Print a welcome to stdout
-			* \return nothing
-			*/
+            DBScan(base::samples::Pointcloud* pointcloud, int min_pts, double epsilon);
+            std::vector<DBScanResult> scan();
+            bool expandCluster(base::Point* start_point, int cluster_id);
 			void welcome();
 	};
+    
+;
 
 } // end namespace machine_learning
 
