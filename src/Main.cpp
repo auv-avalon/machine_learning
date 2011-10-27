1e341a40269ccb0ad1b4ce607493bdfdf270b2e4
Allan E. Conquest <conquest@informatik.uni-bremen.de>
2011-10-27 19:37:24 +0200

    Testing

--- src/Main.cpp
+++ src/Main.cpp
@@ -8,25 +8,25 @@
 int main(int argc, char** argv)
 {
     base::samples::Pointcloud pc;
-	machine_learning::DBScan dbscan;
+	machine_learning::DBScan dbscan(&pc, 2, 2.0); // ignoring depth! use_z = 0
 
     base::Point p1;
-    p1[0] = 1;
-    p1[1] = 2;
+    p1[0] = -1;
+    p1[1] = -2;
     p1[2] = 3;
     pc.points.push_back(p1);
 
     base::Point p2;
-    p2[0] = 6;
-    p2[1] = 7;
-    p2[2] = 8;
+    p2[0] = 1;
+    p2[1] = 2;
+    p2[2] = 3;
     pc.points.push_back(p2);
 
     double dist = dbscan.euclidean_distance(&p1, &p2, false);
     std::cout << "Distance between p1 and p2: " << dist << std::endl;
 
     std::map<base::Point*, int>::iterator it;
-    std::map<base::Point*, int> clustering = dbscan.scan(&pc, 4, 2.0);
+    std::map<base::Point*, int> clustering = dbscan.scan();
 
     std::cout << "Clustering:" << std::endl;
     for(it = clustering.begin(); it != clustering.end(); it++) {
