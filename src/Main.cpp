711a676c26d543df7ee7a0c6b9642184287a6da6
Allan E. Conquest <conquest@informatik.uni-bremen.de>
2011-11-03 23:20:34 +0100

    better testing

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,28 +2,41 @@
 #include <map>
 #include <string>
 #include <sstream>
+#include <cstdlib> // rand()
 #include <machine_learning/DBScan.hpp>
 #include <sonar_detectors/SonarDetectorTypes.hpp>
 
+std::list<sonar_detectors::obstaclePoint>* pointCloud;
+
+std::list<sonar_detectors::obstaclePoint>* generateRandomPointCloud(int size) {
+    delete pointCloud;
+    pointCloud = new std::list<sonar_detectors::obstaclePoint>();
+    int coordRange = 4;
+    for(int i = 0; i < size; i++) {
+        sonar_detectors::obstaclePoint p;
+	p.position[0] = rand() % coordRange;
+        p.position[1] = rand() % coordRange;
+        p.position[2] = rand() % coordRange;
+        pointCloud->push_back(p);
+    }
+    return pointCloud;
+}
+
 int main(int argc, char** argv)
 {
-    std::list<sonar_detectors::obstaclePoint> pl;
-	machine_learning::DBScan dbscan(&pl, 2, 2.0); // ignoring depth! use_z = 0
+    std::cout << "Welcome to the testing of machine_learning" << std::endl;
+    
+    int seed = 1320319343;// time(NULL);
+    std::cout << seed << std::endl;
+    srand(seed);
 
-    sonar_detectors::obstaclePoint p1;
-    p1.position[0] = -1;
-    p1.position[1] = -2;
-    p1.position[2] = 3;
-    pl.push_back(p1);
+    std::list<sonar_detectors::obstaclePoint>* pl = generateRandomPointCloud(10);
+	machine_learning::DBScan dbscan(pl, 2, 2.0); // ignoring depth! use_z = 0
 
-    sonar_detectors::obstaclePoint p2;
-    p2.position[0] = 1;
-    p2.position[1] = 2;
-    p2.position[2] = 3;
-    pl.push_back(p2);
-
+    /*
     double dist = dbscan.euclidean_distance(&p1, &p2, false);
     std::cout << "Distance between p1 and p2: " << dist << std::endl;
+    */
 
     std::map<sonar_detectors::obstaclePoint*, int>::iterator it;
     std::map<sonar_detectors::obstaclePoint*, int> clustering = dbscan.scan();
