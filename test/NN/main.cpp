b70cee3095b7b30a46e9328c612c61030e25aa28
Chris Mueller <christoph.mueller@dfki.de>
2012-03-19 01:53:44 +0100

    add simple test programm for checking the neural network.

--- test/NN/main.cpp
+++ test/NN/main.cpp
@@ -1,6 +1,7 @@
 #include <machine_learning/NeuralNetwork.hpp>
 #include <machine_learning/Data.hpp>
 #include <stdlib.h>
+#include <math.h>
 #include <iostream>
 #include <fstream>
 
@@ -8,20 +9,57 @@ using namespace machine_learning;
 
 int main(int argc, char** argv)
 {
-    NeuralLayer* input = new NeuralLayer(7);
+    if(argc != 2) {
+        std::cout << "Command: sinus_learner <DATAFILE>" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    NeuralLayer* input = new NeuralLayer(10);
     NeuralLayer* output = new NeuralLayer(1, 1.0, NeuralLayer::LINEAR);
     input->connect_to(output);
 
-    NeuralNetwork nn(1, 1.0, input, output);
+    NeuralNetwork nn(2, 1.0, input, output);
+
+    std::fstream file(argv[1]);
+
+    Data input_data(file);
+    Data output_data(floor(1.0 / 0.01), 2);
+
+    double conv = 0;
+
+    std::cerr << std::endl;
+
+    do {
+        conv = nn.theta_sum();
+        for(unsigned i = 0; i < input_data.rows(); i++) {
+            Vector x(2, 1);
+            x << 1.0, input_data(i, 0);
+            
+            Vector t = input_data.block(i, input_data.cols() - 1, 1, 1);
+
+            nn.back_propagation(x, t, 0.01);
+        }
+        fprintf(stderr, "\r Current error %.7f", fabs(conv - nn.theta_sum()));
+    } while(fabs(conv - nn.theta_sum()) > 0.00001);
+
+    std::cerr << std::endl;
+
+    unsigned row = 0;
+    for(double x = 0.0; x <= 1.0; x += 0.01) {
+        Vector r(2, 1);
+        r << 1.0, x;
+
+        Vector y = nn.forward_propagation(r);
+
+        output_data(row, 0) = x;
+        output_data(row, 1) = y(0);
+
+        row++;
+    }
 
 
-    std::fstream file("sin.data");
+    std::cout << output_data << std::endl;
 
-    Data d;
-
-    file >> d;
-
-    std::cout << d << std::endl;
 
     delete input;
     delete output;
